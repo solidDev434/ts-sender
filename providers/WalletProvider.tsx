fe86ffc9f1cc71c7a48a49ca743784228c7d74af
Joseph Ibok <josephibok75@gmail.com>
2025-11-07 17:30:28 +0100

    Integrated Rainbowkit Connect wallet

---
+++ providers/WalletProvider.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import "@rainbow-me/rainbowkit/styles.css";
+import { getDefaultConfig, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { WagmiProvider } from "wagmi";
+import { anvil, zksync } from "wagmi/chains";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+
+const projectID = process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID!;
+
+const config = getDefaultConfig({
+  appName: "TSender",
+  projectId: projectID,
+  chains: [anvil, zksync],
+  ssr: false,
+});
+
+const queryClient = new QueryClient();
+
+export const WalletProvider: React.FC<{
+  children: React.ReactNode;
+}> = ({ children }) => {
+  return (
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <RainbowKitProvider>{children}</RainbowKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
+  );
+};
